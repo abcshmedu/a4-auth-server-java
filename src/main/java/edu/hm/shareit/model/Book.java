2f3f33d14d8dc3dd7d891cfb984b1e9a16ee6374
octav <behrmann@hm.edu>
2017-04-27 16:42:18 +0200

    Added functionality.

--- src/main/java/edu/hm/shareit/model/Book.java
+++ src/main/java/edu/hm/shareit/model/Book.java
@@ -134,18 +134,36 @@ public class Book extends Medium {
     }
 
     private boolean isValidIsbnThirteen(String sanitizedIsbn) {
+        // todo test
         char[] arr = sanitizedIsbn.toCharArray();
+        if (arr.length != 13) {
+            return false;
+        }
 
-        //todo
+        int sum = 0;
+        for (int i = 0; i < 12; i++) {
+            if (i % 2 == 0) {
+                sum += Character.getNumericValue(arr[i]) * 3;
+            } else {
+                sum += Character.getNumericValue(arr[i]);
+            }
+        }
 
-        return true;
+        return (10 - (sum % 10)) == arr[12];
     }
 
     private boolean isValidIsbnTen(String sanitizedIsbn) {
+        // todo test
         char[] arr = sanitizedIsbn.toCharArray();
+        if (arr.length != 10) {
+            return false;
+        }
 
-        //todo
+        int sum = 0;
+        for (int i = 1; i < 9; i++) {
+            sum += Character.getNumericValue(arr[i - 1]) * i;
+        }
 
-        return true;
+        return sum % 11 == Character.getNumericValue(arr[9]);
     }
 }
